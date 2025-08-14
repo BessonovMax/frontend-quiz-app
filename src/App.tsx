d5eea45886ee84bae645966883af1d9198c4df74
Maks Bessonov <max8essonov@gmail.com>
2025-08-14 16:46:03 +0500

    merged template's init files with the challange's starter files

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      
+      {/* <!-- Quiz menu start -->*/}
+      Welcome to the Frontend Quiz! Pick a subject to get started. HTML CSS
+      JavaScript Accessibility
+      {/*<!-- Quiz menu end --> */}
+      {/*<!-- Quiz question start --> */}
+      Question
+      {/*<!-- number --> */}
+      of 10 A B C D Submit answer
+      {/*<!-- Quiz question end --> */}
+      {/*<!-- Quiz completed start --> */}
+      Quiz completed You scored...
+      {/*<!-- score --> */}
+      out of 10
+      {/*<!-- Quiz completed end --> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
