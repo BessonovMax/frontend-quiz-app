e0b92beefee22a49c282ee5eb45986ec16bd446e
Maks Bessonov <max8essonov@gmail.com>
2025-08-19 12:16:35 +0500

    refactor: simplify logic for answered and unanswered states in VariantButton component

--- src/components/VariantButton.tsx
+++ src/components/VariantButton.tsx
@@ -19,30 +19,28 @@ const VariantButton = ({
   letter,
   onClick,
 }: Props) => {
-  const isSelectedAndWrong = isAnswered && isActive && isCorrect === false;
-  const isSelectedAndCorrect = isAnswered && isActive && isCorrect === true;
+  const isSelectedAndWrong = isActive && isCorrect === false;
+  const isSelectedAndCorrect = isActive && isCorrect === true;
 
   // This is for revealing the correct answer if the user chose wrong
-  const isTheRevealedAnswer = isAnswered && !isActive && isCorrect === true;
+  const isTheRevealedAnswer = !isActive && isCorrect === true;
 
   return (
     <button
       className={clsx(
         // --- Base classes that are ALWAYS applied ---
-        "bg-variant-background hover:border-primary border-variant-background group flex min-w-[20.4375rem] items-center justify-between gap-4 rounded-[12px] border-3 p-[13px] hover:cursor-pointer md:min-w-[40rem] md:gap-8 md:rounded-[24px] xl:min-w-[35.25rem] xl:p-[21px]",
-        {
-          // --- Logic for UNANSWERED state ---
-          "!border-primary": isActive && !isAnswered,
-          // State: The user has answered, and all the options are not active
-          "hover:border-variant-background": isAnswered,
-        },
-        // --- Logic for ANSWERED states ---
-        {
-          // State: This was the WRONG answer and the user selected it
-          "!border-incorrect": isSelectedAndWrong,
-          // State: This was the CORRECT answer and the user selected it
-          "!border-correct": isSelectedAndCorrect,
-        },
+        "bg-variant-background group flex min-w-[20.4375rem] items-center justify-between gap-4 rounded-[12px] border-3 p-[13px] hover:cursor-pointer md:min-w-[40rem] md:gap-8 md:rounded-[24px] xl:min-w-[35.25rem] xl:p-[21px]",
+        isAnswered
+          ? {
+              "border-incorrect": isSelectedAndWrong,
+              "border-correct": isSelectedAndCorrect,
+              "border-variant-background":
+                !isSelectedAndWrong && !isSelectedAndCorrect,
+            }
+          : {
+              "border-primary": isActive,
+              "border-variant-background hover:border-primary": !isActive,
+            },
       )}
       onClick={onClick}
       disabled={isAnswered ? true : false}
@@ -52,23 +50,21 @@ const VariantButton = ({
           // --- Base classes that are ALWAYS applied ---
           "flex size-[2.5rem] items-center justify-center rounded-[6px] text-[1.125rem] uppercase md:size-[3.5rem] md:rounded-[12px] md:text-[1.75rem] xl:rounded-[8px]",
 
-          // --- Logic for UNANSWERED states ---
-          {
-            // State: The user has NOT answered yet
-            "bg-variant-foreground text-text-letter group-hover:bg-primary group-hover:text-white":
-              !isAnswered,
-            // State: The user has NOT answered yet, and this is the currently selected option
-            "!bg-primary text-white": isActive && !isAnswered,
-          },
-
-          // --- Logic for ANSWERED states ---
-          {
-            // State: This was the WRONG answer and the user selected it
-            "!bg-incorrect text-white": isSelectedAndWrong,
-            // State: This was the CORRECT answer and the user selected it
-            "!bg-correct text-white":
-              isSelectedAndCorrect || isTheRevealedAnswer,
-          },
+          isAnswered
+            ? {
+                "bg-incorrect text-white": isSelectedAndWrong,
+                "bg-correct text-white":
+                  isSelectedAndCorrect || isTheRevealedAnswer,
+                "bg-variant-foreground text-text-letter":
+                  !isSelectedAndWrong &&
+                  !isSelectedAndCorrect &&
+                  !isTheRevealedAnswer,
+              }
+            : {
+                "bg-primary text-white": isActive,
+                "bg-variant-foreground text-text-letter group-hover:bg-primary group-hover:text-white":
+                  !isActive,
+              },
         )}
       >
         {letter}
